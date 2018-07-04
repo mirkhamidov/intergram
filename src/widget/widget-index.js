a3eb508e75ca24ecae9ac7e463779f82eba8bb36
Джасур Мирхамидов <mirkhamidov.jasur@gmail.com>
2018-07-04 22:30:52 +0300

    host changed

--- src/widget/widget-index.js
+++ src/widget/widget-index.js
@@ -16,7 +16,7 @@ function injectChat() {
         root.id = 'intergramRoot';
         document.getElementsByTagName('body')[0].appendChild(root);
         // const server = window.intergramServer || 'https://www.intergram.xyz';
-        const server = window.intergramServer || 'https://mintergram.herokuapp.com/';
+        const server = window.intergramServer || 'https://mintergram.herokuapp.com';
         const iFrameSrc = server + '/chat.html';
         const host = window.location.host || 'unknown-host';
         const conf = { ...defaultConfiguration, ...window.intergramCustomizations };
